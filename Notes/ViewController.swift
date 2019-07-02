750cbc4fdf5e50fc26c2a47d6fe18bb48429a0a1
Ridwan Abdurrasyid <ridwan.h.abdurrasyid@gmail.com>
2019-07-02 16:44:54 +0700

    Initial Commit

---
+++ Notes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Notes
+//
+//  Created by Ridwan Abdurrasyid on 02/07/19.
+//  Copyright © 2019 Mentimun Mulus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
